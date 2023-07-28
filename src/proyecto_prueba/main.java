02c379479883faf2cdbf2ed312ab27c9e67c87be
JooGo01 <joseakd136@gmail.com>
2023-07-28 20:29:20 -0300

    Segundo commit and push

--- src/proyecto_prueba/main.java
+++ src/proyecto_prueba/main.java
@@ -7,6 +7,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola esto es una prueba para ver si me funciona esto");
+		System.out.println("Esto es para el segundo commit ahre");
 	}
 
 }
