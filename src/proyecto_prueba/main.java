69dd856018f32c23384712d299b2cf85173fb44a
JooGo01 <joseakd136@gmail.com>
2023-07-28 20:25:45 -0300

    Prueba Commit

---
+++ src/proyecto_prueba/main.java
@@ -0,0 +1,12 @@
+package proyecto_prueba;
+
+import java.io.Console;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola esto es una prueba para ver si me funciona esto");
+	}
+
+}
